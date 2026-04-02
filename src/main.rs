194506b9832222a8776e088d5405bbd177e31f81
apaul02 <captainankan@gmail.com>
2026-04-02 19:15:18 +0530

    done ig

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::os::unix::process::CommandExt;
+use std::process;
 use std::{env, path::PathBuf, process::Command};
 
 use nix::mount::{MsFlags, mount};
@@ -18,9 +19,7 @@ fn main() {
 
     match x {
         Some(cmd) => match cmd.as_str() {
-            "run" => {
-                let _child = run(current_exe, a).wait().expect("Failed to wait");
-            }
+            "run" => run(current_exe, a),
             "child" => child(current_exe, a),
             "init" => init(a),
             _ => println!("Error whith the command"),
@@ -31,12 +30,32 @@ fn main() {
     }
 }
 
-pub fn run(binary: PathBuf, args: Vec<String>) -> std::process::Child {
-    Command::new(binary)
+pub fn run(binary: PathBuf, args: Vec<String>) {
+    let id = std::process::id().to_string();
+
+    let container_dir = format!("./containers/{}", id);
+    let upper = format!("{}/upper", container_dir);
+    let work = format!("{}/work", container_dir);
+    let merged = format!("{}/merged", container_dir);
+    let dirs = [&upper, &work, &merged];
+    for dir in dirs {
+        if let Err(e) = std::fs::create_dir_all(dir) {
+            println!("Failed to created dirs: {}", e);
+        }
+    }
+    let mut child = Command::new(binary)
         .arg("child")
+        .arg(&id)
         .args(args)
         .spawn()
-        .expect("Failed to spawn")
+        .expect("Failed to spawn");
+
+    child.wait().expect("Failed to wait on child");
+
+    println!("Container has stopped. Cleaning up {}..", container_dir);
+    if let Err(e) = std::fs::remove_dir_all(&container_dir) {
+        println!("Failed to remove container directory: {}", e);
+    }
 }
 
 pub fn child(binary: PathBuf, args: Vec<String>) {
@@ -74,7 +93,8 @@ pub fn child(binary: PathBuf, args: Vec<String>) {
 
     Command::new(binary)
         .arg("init")
-        .args(args)
+        .arg(&args[0])
+        .args(&args[1..])
         .spawn()
         .expect("failed to spawn init")
         .wait()
@@ -82,14 +102,31 @@ pub fn child(binary: PathBuf, args: Vec<String>) {
 }
 
 pub fn init(args: Vec<String>) {
-    nix::unistd::chroot("./rootfs").unwrap();
+    let id = &args[0];
+
+    let container_dir = format!("./containers/{}", id);
+    let merged = format!("{}/merged", container_dir);
+    let overlay_data = format!(
+        "lowerdir=./rootfs,upperdir={}/upper,workdir={}/work",
+        container_dir, container_dir
+    );
+    if let Err(e) = mount(
+        Some("overlay"),
+        merged.as_str(),
+        Some("overlay"),
+        MsFlags::empty(),
+        Some(overlay_data.as_str()),
+    ) {
+        println!("Error occured: {}", e);
+    }
+    nix::unistd::chroot(merged.as_str()).unwrap();
     nix::unistd::chdir("/").unwrap();
 
     let flags = MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV;
 
     mount(Some("proc"), "/proc", Some("proc"), flags, None::<&str>).unwrap();
 
-    let cmd = &args[0];
+    let cmd = &args[1];
 
-    Command::new(cmd).args(&args[1..]).exec();
+    let _ = Command::new(cmd).args(&args[2..]).exec();
 }
